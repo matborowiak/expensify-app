01107068c08caadbd799ee9a028ac1e319b5d7c2
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-07-27 00:22:10 +0200

    Set up 4 new routes using react-router, setup webpack dev-server historyApiFallback: true

--- src/app.js
+++ src/app.js
@@ -4,9 +4,38 @@ import { BrowserRouter, Route} from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const ExpenseDashboardPage = () => (
+    <div>
+    This is from my dashboard component
+    </div>
+)
+
+const AddExpensePage = () => (
+    <div>
+    This is from my ADD EXPENSE component
+    </div>
+)
+
+const EditExpensePage = () => (
+    <div>
+    This is from my EDIT EXPENSE component
+    </div>
+)
+
+const HelpPage = () => (
+    <div>
+    This is from my HELP PAGE component
+    </div>
+)
+
 const routes = (
     <BrowserRouter>
-
+    <div>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+    </div>
     </BrowserRouter>
 )
 
