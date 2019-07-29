bf16e74dd16e7664ff01303ce6a986eb59636253
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-07-29 13:24:45 +0200

    Added ES6 array destructruring code in playground

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,30 +1,40 @@
-const person = {
-    name: 'Mateusz',
-    age: 29,
-    location: {
-        city: 'Berlin',
-        temp: 34    
-    }
-}
+// Object Destructuring
+// const person = {
+//     name: 'Mateusz',
+//     age: 29,
+//     location: {
+//         city: 'Berlin',
+//         temp: 34    
+//     }
+// }
 
-const {name = 'Anonymous', age} = person
+// const {name = 'Anonymous', age} = person
 
-console.log(`${name} is ${age}.`)
+// console.log(`${name} is ${age}.`)
 
-const { city, temp: temperature } = person.location
+// const { city, temp: temperature } = person.location
 
-if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}`)
-}
+// if (city && temperature) {
+    // console.log(`It's ${temperature} in ${city}`)
+// }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-}
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// }
 
-const {name: publisherName = 'Self-Published'} = book.publisher
+// const {name: publisherName = 'Self-Published'} = book.publisher
 
-console.log(publisherName)
+// console.log(publisherName)
+
+//Array Destructuring
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147']
+const [, , state = 'New York'] = address
+console.log(`You are in ${state}`)
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75']
+const [itemName, , mediumPrice] = item
+console.log(`A medium ${itemName} costs ${mediumPrice}`)
