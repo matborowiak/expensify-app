cb93d474b1db4c02d16dd884eea9db05baf7f459
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-08-20 22:24:15 +0200

    Installed moment.js and react-dates

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import moment from 'moment'
+import { SingleDatePicker } from 'react-dates'
+import 'react-dates/lib/css/_datepicker.css'
+
+const now = moment()
+console.log(now.format('MMM Do, YYYY'))
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        focussed: false
     }
     onDescriptionChange = e => {
         const description = e.target.value
@@ -15,12 +23,11 @@ export default class ExpenseForm extends React.Component {
         const note = e.target.value
         this.setState(() => ({ note }))
     }
-    onAmountChange = (e) => {
+    onAmountChange = e => {
         const amount = e.target.value
         if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }))
         }
-        
     }
     render() {
         return (
@@ -38,6 +45,15 @@ export default class ExpenseForm extends React.Component {
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt} // momentPropTypes.momentObj or null
+                        onDateChange={createdAt => this.setState({ createdAt })} // PropTypes.func.isRequired
+                        focused={this.state.focused} // PropTypes.bool
+                        onFocusChange={({ focused }) =>
+                            this.setState({ focused })
+                        } // PropTypes.func.isRequired
+                        id="your_unique_id" // PropTypes.string.isRequired,
                     />
                     <textarea
                         value={this.state.note}
