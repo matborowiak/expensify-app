aa09c36dc1b991b49406a27295ec3fd1b847403d
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-08-20 18:04:44 +0200

    Added input string filtering, remove button, sort filter list

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const store = configureStore()
+store.dispatch(
+    addExpense({ description: 'Water bill', amount: 100, createdAt: -28000 })
+)
 
-console.log(store.getState())
+store.dispatch(
+    addExpense({ description: 'Gas bill', amount: 4500, createdAt: 1000 })
+)
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+store.dispatch(
+    addExpense({ description: 'Rent', amount: 109500, createdAt: -25000 })
+)
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+
+const jsx = (
+    <Provider store ={store}>
+        <AppRouter />
+    </Provider>
+)
+ReactDOM.render(jsx, document.getElementById('app'))
