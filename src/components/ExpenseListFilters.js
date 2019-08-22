6baecbf1e7b1db3f828ee2284cdd2bac41ba825a
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-08-22 18:41:34 +0200

    Added airbnb date picker for a date based filtering

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,32 +1,60 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters'
+import { DateRangePicker } from 'react-dates'
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../actions/filters'
 
-const ExpenseListFilters = props => (
-    <div>
+class ExpenseListFilters extends React.Component {
+    state = {
+        calendarFocused: null
+    }
+
+    onDatesChange = ({ startDate, endDate }) => {
+        this.props.dispatch(setStartDate(startDate))
+        this.props.dispatch(setEndDate(endDate))
+    }
+
+    onFocusChange = (calendarFocused) => {
+        this.setState(() => ({ calendarFocused }))
+    }
+
+    render() {
+        return (
+            <div>
         <input
             type="text"
-            value={props.filters.text}
+            value={this.props.filters.text}
             onChange={e => {
-                props.dispatch(setTextFilter(e.target.value))
+                this.props.dispatch(setTextFilter(e.target.value))
                 console.log(e.target.value)
             }}
         />
         <select
-            value={props.filters.sortBy}
+            value={this.props.filters.sortBy}
             onChange={e => {
                 if (e.target.value === 'date') {
-                    props.dispatch(sortByDate())
+                    this.props.dispatch(sortByDate())
                 } else if (e.target.value === 'amount') {
-                    props.dispatch(sortByAmount())
+                    this.props.dispatch(sortByAmount())
                 }
             }}
         >
             <option value="date">Date</option>
             <option value="amount">Amount</option>
         </select>
+        <DateRangePicker
+            startDate={this.props.filters.startDate}
+            endDate={this.props.filters.endDate}
+            onDatesChange={this.onDatesChange} // PropTypes.func.isRequired,
+            focusedInput={this.state.calendarFocused} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+            onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
+            showClearDates={true}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+        />
     </div>
-)
+        )
+    }
+}
 
 const mapStateToProps = state => {
     return {
--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,9 +1,12 @@
+import moment from 'moment'
+
+// Filters Reducer
 
 const filtersReducerDefaultState = {
     text: '',
     sortBy: 'date',
-    startDate: undefined,
-    endDate: undefined
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month')
 }
 
 // Filters Reducer
