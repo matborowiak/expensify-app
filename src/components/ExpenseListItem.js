cf78d82dde003e5eacf14207f78b7248bfe3ff19
Mateusz Borowiak <m.borowiak@hotmail.com>
2020-01-24 23:35:19 +0100

    add tests

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 
-const ExpenseListItem = ({description, amount, createdAt, id}) => (
+export const ExpenseListItem = ({description, amount, createdAt, id}) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
